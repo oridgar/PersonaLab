24e955ac0c80cf387ccf7ce81723e492ae130cf6
Or Idgar <idgar@virtualoco.com>
2017-07-13 08:41:57 -0400

    fix setup.py file

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ Created on Jul 13, 2017
 
 @author: idgar
 '''
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='personal-lab',
+setup(name='personalab',
       version='0.1',
       description='personal cloud lab',
       url='http://github.com/oridgar/personalab',
       author='Or Idgar',
       author_email='idgar@virtualoco.com',
       license='GPL',
-      packages=['personal-lab'],
-      install_requires=['SQLAlchemy'],
+      packages=find_packages(),
+      install_requires=['SQLAlchemy','os_client_config','docker','python-openstackclient'],
       zip_safe=False)
