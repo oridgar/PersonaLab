81aae8b326570d97be2ee6c077388737df383dcb
Or Idgar <idgar@virtualoco.com>
2017-07-13 16:10:35 +0300

    some other small fixes

--- setup.py
+++ setup.py
@@ -13,5 +13,9 @@ setup(name='personalab',
       author_email='idgar@virtualoco.com',
       license='GPL',
       packages=find_packages(),
-      install_requires=['SQLAlchemy','os_client_config','docker','python-openstackclient'],
+      install_requires=['SQLAlchemy','os_client_config','docker-py','python-openstackclient'],
       zip_safe=False)
+
+# TODO make sure creating db directory. you should put the db file in the config file
+# also, libvirt and docker should be installed with yum if you want to use them.
+# you need gcc to install python-openstackclient
